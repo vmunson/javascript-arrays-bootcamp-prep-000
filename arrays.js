2658c3de64146b287bfc909f022d83e7d4aa51fc
Ginny Munson <virginia.munson@gmail.com>
2017-11-08 21:26:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,10 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array, element){
   return [element, ...array]
 }
+<<<<<<< HEAD
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   return array.unshift("element")
 }
+=======
+>>>>>>> 005740abce5be7302834083a4c3ca9fb645e6ff0
